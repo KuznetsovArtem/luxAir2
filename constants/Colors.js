8735a7ffe84b592dc1eb667b855859c77987a78e
artem <artem@gypost.com>
2017-10-09 19:38:33 +0300

    Init app

---
+++ constants/Colors.js
@@ -0,0 +1,15 @@
+// const tintColor = '#2f95dc';
+const tintColor = '#1f3b77';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons, Entypo } from '@expo/vector-icons';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+
+import Colors from '../constants/Colors';
+
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import AirHome from '../screens/AirHome';
+
+export default TabNavigator(
+  {
+	  AirHome: {
+		  screen: AirHome,
+	  },
+    Home: {
+      screen: HomeScreen,
+    },
+    Links: {
+      screen: LinksScreen,
+    },
+    Settings: {
+      screen: SettingsScreen,
+    },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        
+        if(routeName === 'AirHome') {
+          return (
+	          <Entypo
+	            name={'aircraft'}
+	            size={28}
+	            style={{ marginBottom: -3 }}
+	            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+	          />
+          );
+        }
+        
+        let iconName;
+        switch (routeName) {
+          case 'AirHome':
+            iconName = 'logo-usd';
+            break;
+          case 'Home':
+            iconName = Platform.OS === 'ios'
+              ? `ios-information-circle${focused ? '' : '-outline'}`
+              : 'md-information-circle';
+            break;
+          case 'Links':
+            iconName = Platform.OS === 'ios'
+              ? `ios-link${focused ? '' : '-outline'}`
+              : 'md-link';
+            break;
+          case 'Settings':
+            iconName = Platform.OS === 'ios'
+              ? `ios-options${focused ? '' : '-outline'}`
+              : 'md-options';
+        }
+        return (
+          <Ionicons
+            name={iconName}
+            size={28}
+            style={{ marginBottom: -3 }}
+            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+          />
+        );
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
